a24d8ab16e68f91f431a518ca27eee7f1feec1e3
wensiannn <gohwensian@gmail.com>
2020-06-12 02:43:20 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,33 +1,14 @@
-
-
 <?php
-/**
- * A class file to connect to database
- */
-class DB_CONNECT {
-  
-  
-    /**
-     * Function to connect with the database
-     */
-    function connectDB() {
-    
-    define('DB_USER', "bc6c9bf48b4bdc"); // db user
-    define('DB_PASSWORD', "ab911a61"); // db password (mention your db password here)
-    define('DB_DATABASE', ""); // database name
-    define('DB_SERVER', "us-cdbr-east-05.cleardb.net"); // db server
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "bc6c9bf48b4bdc";
+$password = "ab911a61";
 
-
-
-//$connect = new PDO('mysql:host=fdb22.atspace.me;dbname=2850356_mydb', '2850356_mydb', '1234m7890');
-        // Connecting to mysql database
-        $con = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE,DB_USER, DB_PASSWORD);
-    
-        
-      
-        // returning connection cursor
-        return $con;
-    }
-   
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=heroku_837ba537cdc8b69", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 ?>
