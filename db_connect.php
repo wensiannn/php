c61c262ce0aa17baeed1cbde70967be1e33f2170
wensiannn <gohwensian@gmail.com>
2020-06-12 16:06:31 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,33 @@
+
+
 <?php
-$servername = "us-cdbr-east-05.cleardb.net";
-$username = "bc6c9bf48b4bdc";
-$password = "ab911a61";
+/**
+ * A class file to connect to database
+ */
+class DB_CONNECT {
+  
+  
+    /**
+     * Function to connect with the database
+     */
+    function connectDB() {
+    
+    define('DB_USER', "bc6c9bf48b4bdc"); // db user
+    define('DB_PASSWORD', "ab911a61"); // db password (mention your db password here)
+    define('DB_DATABASE', "heroku_837ba537cdc8b69"); // database name
+    define('DB_SERVER', "us-cdbr-east-05.cleardb.net"); // db server
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+
+//$connect = new PDO('mysql:host=fdb22.atspace.me;dbname=2850356_mydb', '2850356_mydb', '1234m7890');
+        // Connecting to mysql database
+        $con = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE,DB_USER, DB_PASSWORD);
+    
+        
+      
+        // returning connection cursor
+        return $con;
+    }
+   
 }
-echo "Connected successfully";
 ?>
-
