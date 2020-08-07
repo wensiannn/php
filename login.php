268f7eef1d878a5218ea02a2122c737e146803de
wensiannn <gohwensian@gmail.com>
2020-08-07 14:38:26 +0800

    x

--- login.php
+++ login.php
@@ -1,60 +1,121 @@
-<html><head><body>
-
 <?php
-
-
-  // include db connect class
-    require_once __DIR__ . '/db_connect.php';
-      // connecting to db
-    $db = new DB_CONNECT();
-    $connect = $db->connectDB();
-    
-    if(isset($_POST['ID']) and isset($_POST['Pass'])) {
-
-
-$User = $_POST['ID'];
-$Pass = $_POST['Pass'];
-$query="SELECT * FROM users ";
-$statement = $connect->prepare($query);
-$statement->execute();
-$result = $statement->fetchAll();
-
-foreach($result as $row)
-{
-if($Pass ==$row["password"])
-{
-  echo("Welcome, Click ok to continue");
-  ?><br><a href = "main.html" class = "collection-item"> Ok </a><?php
- 
-
-  }
-  else
-  {
-  echo("Wrong password");
-  }
-  
-}
-
-
-
-
-}
- else
- {
+   ob_start();
+   session_start();
 ?>
 
- 
-<form action= 'login.php' method ='post'>
-  Username:<br>
-  <input type="text" name="ID"><br>
-  Password:<br>
-  <input type="password" name="Pass"><br><br>
-  <input type="submit" value="Login">
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
 
-</form>
-
-<?php
+<html lang = "en">
+   
+   <head>
+     
+      <link href = "css/bootstrap.min.css" rel = "stylesheet">
+      
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: white;
+         }
+         
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+         
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+         
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+         
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+         
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+         
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+         
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+         
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>
+      
+   </head>
+	
+   <body>
+      
+      <h2><centre>Enter Username and Password</h2> 
+      <div class = "container form-signin">
+         
+         <?php
+        if(count($_POST)>0) {
+	if( $_POST["username"] == "admin" and $_POST["password"] == "admin") {
+		$_SESSION["user_id"] = 1001;
+		$_SESSION["username"] = $_POST["username"];
+		$_SESSION['loggedin_time'] = time();  
+                 echo("<meta http-equiv = 'refresh' content = '3; url = main.php' />");
+               echo("Welcome,this page will be redirecting in 3 seconds");
+	} else {
+		$message = "Invalid Username or Password!";
+                echo($message);
+	}
 }
 
-?>
-</body></head></html>
+
+
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username" 
+               required autofocus><br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password" required><br>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button><br>
+         
+		
+       
+         </form>
+      </div> 
+      
+      
+   </body>
+</html>
