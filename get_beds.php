6ad94d5c14aa758c39c1b6a52a57e59d22330d36
wensiannn <gohwensian@gmail.com>
2020-09-29 14:24:00 +0800

    finall

--- get_beds.php
+++ get_beds.php
@@ -1,7 +1,20 @@
+<?php
+session_start();
 
 
+if($_SESSION["username"]=='admin') {
+?>
+<p>Welcome <?php echo $_SESSION["username"]; ?>. Click  <a href="logout.php" tite="Logout">here</a> to Logout.</p>
+<?php
+$login= 1;
+}
 
+else{
+header("Location:dashboard.php");
+//echo ("Please login first, Click here to <a href='login.php' tite='Login'>Log in.");
+}
 
+?>
 
 <!DOCTYPE html>  
 <html>  
@@ -21,7 +34,38 @@
       <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js">  
       </script>             
       <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">  
-      </script>  
+      </script>
+      
+      
+      
+      
+      
+      
+      
+
+             <style type="text/css">
+             
+.myTable { background-color:white;border-collapse:collapse; }
+.myTable th {background-color:#DC143C;border-collapse:collapse; }
+.myTable td, .myTable th { padding:10px;border:2px solid #000; }
+
+</style>
+
+<style>
+body {
+  background-image: url('image/backgrnd2.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+
+
+
+<table class="myTable">
+<tr>
+
+
       
       <style>
 table, th, td {
@@ -34,10 +78,10 @@ table, th, td {
 <html>
 <body>
  <body class="container">
-    <div class = "card-panel teal lighten-2"><h3><p class = "center-align">Nurse Call Station Dashboard</p></h3></div>
+    <div class = "card-panel teal lighten-4"><h4><p class = "center-align">Nurse Call Station Dashboard</p></h4></div>
         <div class = "collection">
  
-      <li><a href = "main.html" class = "collection-item"><i class="material-icons">keyboard_backspace</i>Go to main page</a>
+      <li><a href = "main.php" class = "collection-item"><i class="material-icons">keyboard_backspace</i>Go to main page</a>
    
       
  
@@ -45,6 +89,7 @@ table, th, td {
      
 
  <script>
+ 
   function green(){
   //ctx.fillStyle = "#00FF00"; 
   //ctx.fillRect(70, 20, 150, 100);
@@ -71,22 +116,22 @@ table, th, td {
 
 
  <?php
+
  /*
  * Following code will list all the products
  */
 
   // include db connect class
-    require_once __DIR__ . '/db_connect.php';
     
+    require_once __DIR__ . '/db_connect.php';    
      // connecting to db
     $db = new DB_CONNECT();
     $connect = $db->connectDB();
-     
-
 
  ///List out to check
- 
 
+
+if($login ==1){
 $query = "select * from `nurse` Order by Bed";
  
 
@@ -95,15 +140,21 @@ $count =0;
 $statement->execute();
 $result = $statement->fetchAll();
 
-?>
+      
+
+
+       
    
- <table border = 1>
- <tr>
+
+ echo("<tr>");
+ 
+     
+     
  
 
 
 
-<?php
+
 
 foreach($result as $Bed){
 
@@ -119,9 +170,10 @@ $count=0;
         if($Bed['Attend']== 1)
         {
          ?>
-            <td><b>Bed <?php echo $Bed['Bed'];?> <br>Name: <?php echo $Bed['Name'];?><br>
-            <a href = "search_bed.php?Bed=<?php echo $Bed['Bed'];?>"><img src="image/Red.gif" width = "30%" height ="20%" ></td></a>
-         
+            <td style="background-color:#DC143C;"><b>Bed <?php echo $Bed['Bed'];?> <br>Name: <?php echo $Bed['Name'];?><br>
+            <a onClick="createPopupWin('http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>' , 
+            'Bed Data', 350, 200);"> <img id="bed" src="image/Red.gif" width = "30%" height ="20%" ></td></a>
+       
         <?php
         $count++;
   
@@ -129,9 +181,10 @@ $count=0;
         elseif($Bed['Attend']==0)
         {    
         ?>
-            <td><b>Bed <?php echo $Bed['Bed'];?> <br>Name: <?php echo $Bed['Name'];?><br>
-            <a href = "search_bed.php?Bed=<?php echo $Bed['Bed'];?>"><img src="image/bedGreen.jpg" width = "30%" height ="20%" ></td></a>
-         
+            <td style="background-color:#00FA9A;"><b>Bed <?php echo $Bed['Bed'];?> <br>Name: <?php echo $Bed['Name'];?><br>
+            <a onClick="createPopupWin('http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>' , 
+            'Bed Data', 350, 200);"> <img id="bed" src="image/bedGreen.jpg" width = "30%" height ="20%" ></td></a>
+       
         <?php
         $count++;
 
@@ -140,11 +193,14 @@ $count=0;
    
       
         
-
-
+}
 
 }
+
+
+
 ?>
+
 
  
       
@@ -156,11 +212,116 @@ $count=0;
          //-->
       </script>
       
+      
+<!--<a href="#" onClick="MyWindow=window.open('http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>''popup'); return false;">Click Here</a>-->
+     
+     <script> 
+        function createPopupWin(pageURL, pageTitle, 
+                    popupWinWidth, popupWinHeight) { 
+            var left = (screen.width ) ; 
+            var top = (screen.height ) ; 
+            var myWindow = window.open(pageURL, pageTitle,  
+                    'resizable=yes, width=' + popupWinWidth
+                    + ', height=' + popupWinHeight + ', top=' 
+                    + ((top/2)-100) + ', left=' + ((left/2)-175)); 
+        } 
+    </script> 
+      
+    <!--<button onclick = "createPopupWin('http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>' , 
+            'Bed Data', 350, 200);"> 
+        Test 
+    </button> -->
 
-   
-   <body onload = "JavaScript:AutoRefresh(1000);">
+   <!--  <a href='http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>' 
+  target="popup" 
+  onclick="window.open('http://nursecall.atspace.cc/search_bed.php?Bed=<?php echo $Bed['Bed'];?>','popup','width=350,height=200,scrollbars=no,resizable=no'); return false;">
+    Open Link in Popup
+</a>
 
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+/* Popup container - can be anything you want */
+.popup {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* The actual popup */
+.popup .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class - hide and show the popup */
+.popup .show {
+  visibility: visible;
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 1s;
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+  from {opacity: 0;} 
+  to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity:1 ;}
+}
+</style>
+</head>
+<body style="text-align:center">
+
+<h2>Popup</h2>
+
+<div class="popup" onclick="myFunction()">Click me to toggle the popup!
+  <span class="popuptext" id="myPopup">Hello</span>
+</div>
+
+<script>
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
+-->
+
+   <body onload = "JavaScript:AutoRefresh(2000);">
+    
    </body>
+   
+
    
 </html>
 
